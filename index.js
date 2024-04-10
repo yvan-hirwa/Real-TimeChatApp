84b899c7d34d66818b8a4985750cd4d40fad6601
hirwkar <hirwakaranganwayvan@gmail.com>
2024-04-10 12:27:52 +0200

    Added functionality

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const {createServer} = require('node:http');
 const app = express();
 const server = createServer(app);
 const {join} = require('node:path');
+const {Server} = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res)=>{
     res.sendFile(join(__dirname, "/index.html"));
+})
+
+io.on('connection', (socket)=>{
+    console.log('A user is connected ...');
+    socket.on('Chat Message', (msg)=>{
+        io.emit('Chat Message', msg);
+    })
 })
 
 server.listen(3000, ()=>{
